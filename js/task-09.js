c797bc543f518d4045821d80a1679b9d9fcd6fec
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-11-07 22:00:20 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
 const buttonEl = document.querySelector('.change-color');
 const spanEl = document.querySelector('.color');
-console.log(spanEl.textContent);
-
-spanEl.textContent = ${changebackground};
 
 function changebackground() {
   document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor()
 }
-
 buttonEl.addEventListener('click', changebackground);
