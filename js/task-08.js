4d2a1b7f10f2aac0adebc9fa6871c0ed06ad88ca
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-11-08 22:24:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,8 +1,56 @@
-const formEl = document.querySelector('.login-form');
+const form = document.querySelector('.login-form');
+const email = document.querySelector('input[name=email]')
+const pass = document.querySelector('input[name=password]')
+const btn = document.querySelector('button[type=submit]')
 
-function message(event) {
+const response = {}
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-}
+  const [email, password] = event.target.elements;
+  if(document.querySelector('input[name=email]').value.length == 0 || document.querySelector('input[name=password]').value.length == 0) {
+    alert("Заполните все поля формы!")
+  }
+  console.log("email :", email.value)
+  console.log("password :", password.value)
+
+  // const formData = new FormData(event.target)
+  // formData.forEach((password, email) => {
+  //   console.log(`${email} : ${password}`)
+  // })
+  form.reset();
+});
 
 
-formEl.addEventListener('submit', formResult);
+// const response = {}
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const formData = new FormData(event.target)
+//   formData.forEach((password, email) => {
+//     console.log(`${email} : ${password}`)
+//   })
+// });
+
+  // console.log('event', event.target.elements)
+
+  // response.email = email.email;
+  // response.password = pass.password;
+  // console.log(response)
+  // console.log(event.target)
+
+// formEl.addEventListener('input', (event) => {
+//   event.preventDefault();
+//   console.log(event.target.value)
+//   const text = document.querySelector('.text')
+
+//   if (event.target.value === "max") {
+//     alert(event.target.value);
+//   }
+
+//   text.innerHTML = event.target.value;
+// });
+
+// + Обработка отправки формы form.login-form должна быть по событию submit.
+// + При отправке формы страница не должна перезагружаться.
+// + Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// +- Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// + Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
